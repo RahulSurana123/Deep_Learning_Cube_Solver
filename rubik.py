425b8035bc06415777b5bba0ac75aa2b06f5908e
Karan <karansoni146.ks@gmail.com>
2021-03-23 11:38:59 +0530

    Committing initial code for Rubik's Cube

---
+++ rubik.py
@@ -0,0 +1,132 @@
+from tkinter import Tk, Canvas, Frame, BOTH
+from tkinter import *
+import random
+
+root=Tk()
+
+root.title("Rubik's Cube Solver")
+canvas=Canvas(width=1080, height=640, background='gray75')
+color = ['red','green','yellow','blue','white','orange',
+		'red','green','yellow','blue','white','orange',
+		'red','green','yellow','blue','white','orange',
+		'red','green','yellow','blue','white','orange',
+		'red','green','yellow','blue','white','orange',
+		'red','green','yellow','blue','white','orange',
+		'red','green','yellow','blue','white','orange',
+		'red','green','yellow','blue','white','orange',
+		'red','green','yellow','blue','white','orange']
+color_box=['red','green','yellow','blue','white','orange']
+
+arr=[[[[]]]]
+
+
+
+
+def phase_of_cube(x,y,color):
+
+	box=canvas.create_rectangle(x, y, x+50, y+50, outline="#000000", fill=color, width=2)
+	box=canvas.create_rectangle(x+50, y, x+100, y+50, outline="#000000", fill=color, width=2)
+	box=canvas.create_rectangle(x+100, y, x+150, y+50, outline="#000000", fill=color, width=2)
+	box=canvas.create_rectangle(x, y+50, x+50, y+100, outline="#000000", fill=color, width=2)
+	box=canvas.create_rectangle(x+50, y+50, x+100, y+100, outline="#000000", fill=color, width=2)
+	box=canvas.create_rectangle(x+100, y+50, x+150, y+100, outline="#000000", fill=color, width=2)
+	box=canvas.create_rectangle(x, y+100, x+50, y+150, outline="#000000", fill=color, width=2)
+	box=canvas.create_rectangle(x+50, y+100, x+100, y+150, outline="#000000", fill=color, width=2)
+	box=canvas.create_rectangle(x+100, y+100, x+150, y+150, outline="#000000", fill=color, width=2)
+
+def box_in_cube(x,y):
+	col=random.randint(0,len(color))
+	box1=canvas.create_rectangle(x, y, x+50, y+50, outline="#000000", fill=color[col], width=2)
+	color.remove(color[col])
+	print(len(color))
+
+
+canvas.pack()
+
+
+#LEFT
+button1=Button(canvas, text="<<",bg="gray")
+button1.place(x=60, y=262)
+button2=Button(canvas, text="<<",bg="gray")
+button2.place(x=60, y=312)
+button3=Button(canvas, text="<<",bg="gray")
+button3.place(x=60, y=362)
+#RIGHT
+button4=Button(canvas, text=">>",bg="gray")
+button4.place(x=562, y=262)
+button5=Button(canvas, text=">>",bg="gray")
+button5.place(x=562, y=312)
+button6=Button(canvas, text=">>",bg="gray")
+button6.place(x=562, y=362)
+#TOP
+button7=Button(canvas, text="^^",bg="gray")
+button7.place(x=262, y=60)
+button8=Button(canvas, text="^^",bg="gray")
+button8.place(x=312, y=60)
+button9=Button(canvas, text="^^",bg="gray")
+button9.place(x=362, y=60)
+#BOTTOM
+button10=Button(canvas, text="vv",bg="gray")
+button10.place(x=262, y=562)
+button11=Button(canvas, text="vv",bg="gray")
+button11.place(x=312, y=562)
+button12=Button(canvas, text="vv",bg="gray")
+button12.place(x=362, y=562)
+
+
+# #PHASES OF THE CUBE
+ 
+# #Top
+# phase_of_cube(250,100,'red')
+# #Right
+# phase_of_cube(400,250,'yellow')
+# #Left
+# phase_of_cube(100,250,'orange')
+# #Bottom
+# phase_of_cube(250,400,'white')
+# #Front
+# phase_of_cube(250,250,'blue')
+# #Back
+# phase_of_cube(750,250,'green')
+
+
+#PHASES OF THE CUBE
+ 
+#Top
+for i1 in range(250,351,50):
+	for j1 in range(100,201,50):
+		print(i1,j1)
+		box_in_cube(i1,j1)
+
+#Right
+for i2 in range(400,501,50):
+	for j2 in range(250,351,50):
+		print(i2,j2)
+		box_in_cube(i2,j2)
+
+#Left
+for i3 in range(100,201,50):
+	for j3 in range(250,351,50):
+		print(i3,j3)
+		box_in_cube(i3,j3)
+
+#Bottom
+for i4 in range(250,351,50):
+	for j4 in range(400,501,50):
+		print(i4,j4)
+		box_in_cube(i4,j4)
+
+#Front
+for i5 in range(250,351,50):
+	for j5 in range(250,351,50):
+		print(i5,j5)
+		box_in_cube(i5,j5)
+
+#Back
+for i6 in range(700,801,50):
+	for j6 in range(250,351,50):
+		print(i6,j6)
+		box_in_cube(i6,j6)
+
+
+root.mainloop()
