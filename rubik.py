27261738861f1a943d70ecfd60eaa98e1bbcbdf9
Rahul Surana <rahul.surana@mavenir.com>
2021-03-25 16:50:00 +0530

    added cube generation code

--- rubik.py
+++ rubik.py
@@ -4,6 +4,15 @@ import random
 from PIL import Image, ImageTk
 
 
+def valid_cube_generation():
+	c = [[[0 for i in range(3)] for j in range(3)]for k in range(6)]
+	for i in edges:
+		ind = random.randint(0,1)
+		# TODO take 2 random edge from edges
+		# and place them in cube at random edge_index
+	return c
+				
+
 def side_displayer(x,y,c33):
 	for i1 in range(3):
 		for j1 in range(3):
@@ -12,22 +21,22 @@ def side_displayer(x,y,c33):
 def display():
 	for i in range(6):
 		if cube[i][1][1] == 0:
-			#Front
+			#Front  White
 			side_displayer(250,250,cube[i])
 		elif cube[i][1][1] == 1:
-			#Back
+			#Back  Yellow
 			side_displayer(700,250,cube[i])
 		elif cube[i][1][1] == 2:
-			#Right
+			#Right Green
 			side_displayer(400,250,cube[i])			
 		elif cube[i][1][1] == 3:
-			#Top
+			#Top Blue
 			side_displayer(250,100,cube[i])
 		elif cube[i][1][1] == 4:
-			#Left
+			#Left Red
 			side_displayer(100,250,cube[i])
 		else:
-			#Bottom
+			#Bottom Orange
 			side_displayer(250,400,cube[i])
 
 def box_in_cube(x,y,c):
@@ -35,30 +44,32 @@ def box_in_cube(x,y,c):
 	while color_dict[col] >= 9:
 		col = random.randint(0,5)
 	color_dict[col] += 1
-	box=canvas.create_rectangle(x, y, x+50, y+50, outline="#666666", fill=color_box[c], width=5)
+	box=canvas.create_rectangle(x, y, x+50, y+50, outline="#ffffff", fill=color_box[c], width=5)
 
 
 root = Tk()
 
 root.title("Rubik's Cube Solver")
 
+
+#Constants number represent color in cube 
+color_box=['black','yellow','green','blue','red','orange']
+edges = [(0,2),(0,3),(0,4),(0,5),(1,2),(1,3),(1,4),(1,5),(2,3),(2,5),(4,3),(4,5)]
+corners = [(0,2,3),(0,3,4),(0,4,5),(0,2,5),(1,2,3),(1,3,4),(1,4,5),(1,2,5)]
+edge_to_cube_map = {}
+
+color_dict = {0:0,1:0,2:0,3:0,4:0,5:0}
+
 canvas=Canvas(width=960, height=640, background='#808080')
 
-image1 = Image.open("New folder/2.jpg")
+image1 = Image.open("New folder/3.jpg")
 test = ImageTk.PhotoImage(image1)
 canvas.create_image(0, 0, image=test, anchor='nw')
 
-cube = [[[0 for i in range(3)] for j in range(3)]for k in range(6)]
-
-for i in range(6):
-	cube[i][1][1] = i
+cube = valid_cube_generation()
 
 for i in range(6):
 	print(cube[i])
-
-color_box=['white','yellow','green','blue','red','orange']
-
-color_dict = {0:0,1:0,2:0,3:0,4:0,5:0}
 
 display()
 
