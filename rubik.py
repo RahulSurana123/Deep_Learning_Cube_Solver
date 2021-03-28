f5770bb123bd94af303e99efdb9ac8757d86c220
Karansoni1910 <karansoni146.ks@gmail.com>
2021-03-28 12:18:09 +0530

    All rotations added as functions and display() is changed

--- rubik.py
+++ rubik.py
@@ -23,25 +23,24 @@ def side_displayer(x,y,c33):
 			box_in_cube((x+50*i1),(y+50*j1),c33[i1][j1])
 
 def display():
-	for i in range(6):
-		if cube[i][1][1] == 0:
-			#Front  Black
-			side_displayer(250,250,cube[i])
-		elif cube[i][1][1] == 1:
-			#Back  Yellow
-			side_displayer(700,250,cube[i])
-		elif cube[i][1][1] == 2:
-			#Right Orange
-			side_displayer(400,250,cube[i])			
-		elif cube[i][1][1] == 3:
-			#Top Blue
-			side_displayer(250,100,cube[i])
-		elif cube[i][1][1] == 4:
-			#Left Red
-			side_displayer(100,250,cube[i])
-		else:
-			#Bottom Green
-			side_displayer(250,400,cube[i])
+	# 0
+	#Front  Black
+	side_displayer(250,250,cube[0])
+	# 1
+	#Back  Yellow
+	side_displayer(700,250,cube[1])
+	# 2
+	#Right Orange
+	side_displayer(400,250,cube[2])			
+	# 3
+	#Top Blue
+	side_displayer(250,100,cube[3])
+	# 4
+	#Left Red
+	side_displayer(100,250,cube[4])
+	# 5
+	#Bottom Green
+	side_displayer(250,400,cube[5])
 
 def box_in_cube(x,y,c):
 	col=random.randint(0,5)
@@ -52,35 +51,461 @@ def box_in_cube(x,y,c):
 	# text=canvas.create_text(x+20, y+20, text = str(i)+str(i1)+str(i2), anchor='center', font='TkMenuFont', fill='white')
 
 
-def rotation_by_phase_clock():
-	i = 0
 
+"""
+COLOR  : INDEX : PHASE
+BLACK  :   0   : FRONT
+GREEN  :   1   : BOTTOM
+ORANGE :   2   : RIGHT
+RED    :   3   : LEFT
+BLUE   :   4   : TOP
+YELLOW :   5   : BACK
+"""
+
+
+
+def rotate_up_column_0():
+	#phase(clockwise)
+	i=0
+	temp = cube[i+4][0][0]
+	cube[i+4][0][0]=cube[i+4][1][0]
+	cube[i+4][1][0]=cube[i+4][2][0]
+	cube[i+4][2][0]=cube[i+4][2][1]
+	cube[i+4][2][1]=cube[i+4][2][2]
+	cube[i+4][2][2]=cube[i+4][1][2]
+	cube[i+4][1][2]=cube[i+4][0][2]
+	cube[i+4][0][2]=cube[i+4][0][1]
+	cube[i+4][0][1]=temp
+
+	for a in range(3):
+		temp = cube[i][0][0]
+		cube[i][0][0]=cube[i][0][1]
+		cube[i][0][1]=cube[i][0][2]
+		cube[i][0][2]=cube[i+5][0][0]
+		cube[i+5][0][0]=cube[i+5][0][1]
+		cube[i+5][0][1]=cube[i+5][0][2]
+		cube[i+5][0][2]=cube[i+1][0][0]
+		cube[i+1][0][0]=cube[i+1][0][1]
+		cube[i+1][0][1]=cube[i+1][0][2]
+		cube[i+1][0][2]=cube[i+3][0][0]
+		cube[i+3][0][0]=cube[i+3][0][1]
+		cube[i+3][0][1]=cube[i+3][0][2]
+		cube[i+3][0][2]=temp
+
+def rotate_down_column_0():
+	# phase(anti-clockwise)
+	i=0
+	temp = cube[i+4][0][0]
+	cube[i+4][0][0]=cube[i+4][0][1]
+	cube[i+4][0][1]=cube[i+4][0][2]
+	cube[i+4][0][2]=cube[i+4][1][2]
+	cube[i+4][1][2]=cube[i+4][2][2]
+	cube[i+4][2][2]=cube[i+4][2][1]
+	cube[i+4][2][1]=cube[i+4][2][0]
+	cube[i+4][2][0]=cube[i+4][1][0]
+	cube[i+4][1][0]=temp
+
+	for a in range(3):
+		temp = cube[i][0][2]
+		cube[i][0][2]=cube[i][0][1]
+		cube[i][0][1]=cube[i][0][0]
+		cube[i][0][0]=cube[i+3][0][2]
+		cube[i+3][0][2]=cube[i+3][0][1]
+		cube[i+3][0][1]=cube[i+3][0][0]
+		cube[i+3][0][0]=cube[i+1][0][2]
+		cube[i+1][0][2]=cube[i+1][0][1]
+		cube[i+1][0][1]=cube[i+1][0][0]
+		cube[i+1][0][0]=cube[i+5][0][2]
+		cube[i+5][0][2]=cube[i+5][0][1]
+		cube[i+5][0][1]=cube[i+5][0][0]
+		cube[i+5][0][0]=temp
+
+
+def rotate_up_column_2():
+	i=0
+	temp = cube[i+2][0][0]
+	cube[i+2][0][0]=cube[i+2][0][1]
+	cube[i+2][0][1]=cube[i+2][0][2]
+	cube[i+2][0][2]=cube[i+2][1][2]
+	cube[i+2][1][2]=cube[i+2][2][2]
+	cube[i+2][2][2]=cube[i+2][2][1]
+	cube[i+2][2][1]=cube[i+2][2][0]
+	cube[i+2][2][0]=cube[i+2][1][0]
+	cube[i+2][1][0]=temp
+
+	for a in range(3):
+		temp = cube[i][2][0]
+		cube[i][2][0]=cube[i][2][1]
+		cube[i][2][1]=cube[i][2][2]
+		cube[i][2][2]=cube[i+5][2][0]
+		cube[i+5][2][0]=cube[i+5][2][1]
+		cube[i+5][2][1]=cube[i+5][2][2]
+		cube[i+5][2][2]=cube[i+1][2][0]
+		cube[i+1][2][0]=cube[i+1][2][1]
+		cube[i+1][2][1]=cube[i+1][2][2]
+		cube[i+1][2][2]=cube[i+3][2][0]
+		cube[i+3][2][0]=cube[i+3][2][1]
+		cube[i+3][2][1]=cube[i+3][2][2]
+		cube[i+3][2][2]=temp
+
+def rotate_down_column_2():
+	i=0
+	temp = cube[i+2][0][0]
+	cube[i+2][0][0]=cube[i+2][0][1]
+	cube[i+2][0][1]=cube[i+2][0][2]
+	cube[i+2][0][2]=cube[i+2][1][2]
+	cube[i+2][1][2]=cube[i+2][2][2]
+	cube[i+2][2][2]=cube[i+2][2][1]
+	cube[i+2][2][1]=cube[i+2][2][0]
+	cube[i+2][2][0]=cube[i+2][1][0]
+	cube[i+2][1][0]=temp
+
+	for a in range(3):
+		temp = cube[i][2][2]
+		cube[i][2][2]=cube[i][2][1]
+		cube[i][2][1]=cube[i][2][0]
+		cube[i][2][0]=cube[i+3][2][2]
+		cube[i+3][2][2]=cube[i+3][2][1]
+		cube[i+3][2][1]=cube[i+3][2][0]
+		cube[i+3][2][0]=cube[i+1][2][2]
+		cube[i+1][2][2]=cube[i+1][2][1]
+		cube[i+1][2][1]=cube[i+1][2][0]
+		cube[i+1][2][0]=cube[i+5][2][2]
+		cube[i+5][2][2]=cube[i+5][2][1]
+		cube[i+5][2][1]=cube[i+5][2][0]
+		cube[i+5][2][0]=temp
+
+def rotate_up_column_1():
+	i=0	
+	for a in range(3):
+		temp = cube[i][1][0]
+		cube[i][1][0]=cube[i][1][1]
+		cube[i][1][1]=cube[i][1][2]
+		cube[i][1][2]=cube[i+5][1][0]
+		cube[i+5][1][0]=cube[i+5][1][1]
+		cube[i+5][1][1]=cube[i+5][1][2]
+		cube[i+5][1][2]=cube[i+1][1][0]
+		cube[i+1][1][0]=cube[i+1][1][1]
+		cube[i+1][1][1]=cube[i+1][1][2]
+		cube[i+1][1][2]=cube[i+3][1][0]
+		cube[i+3][1][0]=cube[i+3][1][1]
+		cube[i+3][1][1]=cube[i+3][1][2]
+		cube[i+3][1][2]=temp
+
+def rotate_down_column_1():
+	i=0	
+	for a in range(3):
+		temp = cube[i][1][2]
+		cube[i][1][2]=cube[i][1][1]
+		cube[i][1][1]=cube[i][1][0]
+		cube[i][1][0]=cube[i+3][1][2]
+		cube[i+3][1][2]=cube[i+3][1][1]
+		cube[i+3][1][1]=cube[i+3][1][0]
+		cube[i+3][1][0]=cube[i+1][1][2]
+		cube[i+1][1][2]=cube[i+1][1][1]
+		cube[i+1][1][1]=cube[i+1][1][0]
+		cube[i+1][1][0]=cube[i+5][1][2]
+		cube[i+5][1][2]=cube[i+5][1][1]
+		cube[i+5][1][1]=cube[i+5][1][0]
+		cube[i+5][1][0]=temp
+
+
+def rotate_right_row_0():
+	#phase(clockwise)
+	i=0
+	temp = cube[i+3][0][0]
+	cube[i+3][0][0]=cube[i+3][0][1]
+	cube[i+3][0][1]=cube[i+3][0][2]
+	cube[i+3][0][2]=cube[i+3][1][2]
+	cube[i+3][1][2]=cube[i+3][2][2]
+	cube[i+3][2][2]=cube[i+3][2][1]
+	cube[i+3][2][1]=cube[i+3][2][0]
+	cube[i+3][2][0]=cube[i+3][1][0]
+	cube[i+3][1][0]=temp
+
+
+	for a in range(3):
+		temp = cube[i][2][0]
+		cube[i][2][0]=cube[i][1][0]
+		cube[i][1][0]=cube[i][0][0]
+		cube[i][0][0]=cube[i+4][2][0]
+		cube[i+4][2][0]=cube[i+4][1][0]
+		cube[i+4][1][0]=cube[i+4][0][0]
+		cube[i+4][0][0]=cube[i+1][2][0]
+		cube[i+1][2][0]=cube[i+1][1][0]
+		cube[i+1][1][0]=cube[i+1][0][0]
+		cube[i+1][0][0]=cube[i+2][2][0]
+		cube[i+2][2][0]=cube[i+2][1][0]
+		cube[i+2][1][0]=cube[i+2][0][0]
+		cube[i+2][0][0]=temp
+
+def rotate_left_row_0():
+	#phase(clockwise)
+	i=0
+	temp = cube[i+3][0][0]
+	cube[i+3][0][0]=cube[i+3][1][0]
+	cube[i+3][1][0]=cube[i+3][2][0]
+	cube[i+3][2][0]=cube[i+3][2][1]
+	cube[i+3][2][1]=cube[i+3][2][2]
+	cube[i+3][2][2]=cube[i+3][1][2]
+	cube[i+3][1][2]=cube[i+3][0][2]
+	cube[i+3][0][2]=cube[i+3][0][1]
+	cube[i+3][0][1]=temp
+
+	for a in range(3):
+		temp = cube[i][0][0]
+		cube[i][0][0]=cube[i][1][0]
+		cube[i][1][0]=cube[i][2][0]
+		cube[i][2][0]=cube[i+2][0][0]
+		cube[i+2][0][0]=cube[i+2][1][0]
+		cube[i+2][1][0]=cube[i+2][2][0]
+		cube[i+2][2][0]=cube[i+1][0][0]
+		cube[i+1][0][0]=cube[i+1][1][0]
+		cube[i+1][1][0]=cube[i+1][2][0]
+		cube[i+1][2][0]=cube[i+4][0][0]
+		cube[i+4][0][0]=cube[i+4][1][0]
+		cube[i+4][1][0]=cube[i+4][2][0]
+		cube[i+4][2][0]=temp
+
+
+def rotate_right_row_2():
+	#phase(clockwise)
+	i=0
+	temp = cube[i+5][0][0]
+	cube[i+5][0][0]=cube[i+5][1][0]
+	cube[i+5][1][0]=cube[i+5][2][0]
+	cube[i+5][2][0]=cube[i+5][2][1]
+	cube[i+5][2][1]=cube[i+5][2][2]
+	cube[i+5][2][2]=cube[i+5][1][2]
+	cube[i+5][1][2]=cube[i+5][0][2]
+	cube[i+5][0][2]=cube[i+5][0][1]
+	cube[i+5][0][1]=temp
+
+	for a in range(3):
+		temp = cube[i][2][2]
+		cube[i][2][2]=cube[i][1][2]
+		cube[i][1][2]=cube[i][0][2]
+		cube[i][0][2]=cube[i+4][2][2]
+		cube[i+4][2][2]=cube[i+4][1][2]
+		cube[i+4][1][2]=cube[i+4][0][2]
+		cube[i+4][0][2]=cube[i+1][2][2]
+		cube[i+1][2][2]=cube[i+1][1][2]
+		cube[i+1][1][2]=cube[i+1][0][2]
+		cube[i+1][0][2]=cube[i+2][2][2]
+		cube[i+2][2][2]=cube[i+2][1][2]
+		cube[i+2][1][2]=cube[i+2][0][2]
+		cube[i+2][0][2]=temp
+
+def rotate_left_row_2():
+	# phase(anti-clockwise)
+	i=0
+	temp = cube[i+5][0][0]
+	cube[i+5][0][0]=cube[i+5][0][1]
+	cube[i+5][0][1]=cube[i+5][0][2]
+	cube[i+5][0][2]=cube[i+5][1][2]
+	cube[i+5][1][2]=cube[i+5][2][2]
+	cube[i+5][2][2]=cube[i+5][2][1]
+	cube[i+5][2][1]=cube[i+5][2][0]
+	cube[i+5][2][0]=cube[i+5][1][0]
+	cube[i+5][1][0]=temp
+
+	for a in range(3):
+		temp = cube[i][0][2]
+		cube[i][0][2]=cube[i][1][2]
+		cube[i][1][2]=cube[i][2][2]
+		cube[i][2][2]=cube[i+2][0][2]
+		cube[i+2][0][2]=cube[i+2][1][2]
+		cube[i+2][1][2]=cube[i+2][2][2]
+		cube[i+2][2][2]=cube[i+1][0][2]
+		cube[i+1][0][2]=cube[i+1][1][2]
+		cube[i+1][1][2]=cube[i+1][2][2]
+		cube[i+1][2][2]=cube[i+4][0][2]
+		cube[i+4][0][2]=cube[i+4][1][2]
+		cube[i+4][1][2]=cube[i+4][2][2]
+		cube[i+4][2][2]=temp
+
+def rotate_right_row_1():
+	i=0	
+	for a in range(3):
+		temp = cube[i][2][1]
+		cube[i][2][1]=cube[i][1][1]
+		cube[i][1][1]=cube[i][0][1]
+		cube[i][0][1]=cube[i+4][2][1]
+		cube[i+4][2][1]=cube[i+4][1][1]
+		cube[i+4][1][1]=cube[i+4][0][1]
+		cube[i+4][0][1]=cube[i+1][2][1]
+		cube[i+1][2][1]=cube[i+1][1][1]
+		cube[i+1][1][1]=cube[i+1][0][1]
+		cube[i+1][0][1]=cube[i+2][2][1]
+		cube[i+2][2][1]=cube[i+2][1][1]
+		cube[i+2][1][1]=cube[i+2][0][1]
+		cube[i+2][0][1]=temp
+
+def rotate_left_row_1():
+	i=0	
+	for a in range(3):
+		temp = cube[i][0][1]
+		cube[i][0][1]=cube[i][1][1]
+		cube[i][1][1]=cube[i][2][1]
+		cube[i][2][1]=cube[i+2][0][1]
+		cube[i+2][0][1]=cube[i+2][1][1]
+		cube[i+2][1][1]=cube[i+2][2][1]
+		cube[i+2][2][1]=cube[i+1][0][1]
+		cube[i+1][0][1]=cube[i+1][1][1]
+		cube[i+1][1][1]=cube[i+1][2][1]
+		cube[i+1][2][1]=cube[i+4][0][1]
+		cube[i+4][0][1]=cube[i+4][1][1]
+		cube[i+4][1][1]=cube[i+4][2][1]
+		cube[i+4][2][1]=temp
+
+
+def rotate_front_clock():
+	i=0
+	temp = cube[i][0][0]
+	cube[i][0][0]=cube[i][1][0]
+	cube[i][1][0]=cube[i][2][0]
+	cube[i][2][0]=cube[i][2][1]
+	cube[i][2][1]=cube[i][2][2]
+	cube[i][2][2]=cube[i][1][2]
+	cube[i][1][2]=cube[i][0][2]
+	cube[i][0][2]=cube[i][0][1]
+	cube[i][0][1]=temp
+
+	for a in range(3):
+		temp = cube[i+4][2][2]
+		cube[i+4][2][2]=cube[i+4][2][1]
+		cube[i+4][2][1]=cube[i+4][2][0]
+		cube[i+4][2][0]=cube[i+5][0][0]
+		cube[i+5][0][0]=cube[i+5][1][0]
+		cube[i+5][1][0]=cube[i+5][2][0]
+		cube[i+5][2][0]=cube[i+2][0][2]
+		cube[i+2][0][2]=cube[i+2][0][1]
+		cube[i+2][0][1]=cube[i+2][0][0]
+		cube[i+2][0][0]=cube[i+3][2][2]
+		cube[i+3][2][2]=cube[i+3][1][2]
+		cube[i+3][1][2]=cube[i+3][0][2]
+		cube[i+3][0][2]=temp
+
+def rotate_front_anti():
+	# phase(anti-clockwise)
+	i=0
 	temp = cube[i][0][0]
 	cube[i][0][0]=cube[i][0][1]
 	cube[i][0][1]=cube[i][0][2]
-	cube[i][0][2]=cube[i][2][1]
+	cube[i][0][2]=cube[i][1][2]
 	cube[i][1][2]=cube[i][2][2]
 	cube[i][2][2]=cube[i][2][1]
 	cube[i][2][1]=cube[i][2][0]
 	cube[i][2][0]=cube[i][1][0]
 	cube[i][1][0]=temp
 
-	temp = cube[(i+3)%6][2][2]
-	cube[(i+3)%6][2][2]=cube[(i+3)%6][1][2]
-	cube[(i+3)%6][1][2]=cube[(i+3)%6][0][2]
-	cube[(i+3)%6][0][2]=cube[(i+4)%6][2][0]
+	for a in range(3):
+		temp = cube[i+2][0][0]
+		cube[i+2][0][0]=cube[i+2][0][1]
+		cube[i+2][0][1]=cube[i+2][0][2]
+		cube[i+2][0][2]=cube[i+5][2][0]
+		cube[i+5][2][0]=cube[i+5][1][0]
+		cube[i+5][1][0]=cube[i+5][0][0]
+		cube[i+5][0][0]=cube[i+4][2][2]
+		cube[i+4][2][2]=cube[i+4][2][1]
+		cube[i+4][2][1]=cube[i+4][2][0]
+		cube[i+4][2][0]=cube[i+3][0][2]
+		cube[i+3][0][2]=cube[i+3][1][2]
+		cube[i+3][1][2]=cube[i+3][2][2]
+		cube[i+3][2][2]=temp
 
-	cube[(i+4)%6][2][0]=cube[(i+4)%6][2][1]
-	cube[(i+4)%6][2][1]=cube[(i+4)%6][2][0]
-	cube[(i+4)%6][2][2]=cube[(i+5)%6][0][0]
-	cube[(i+5)%6][0][0]=cube[(i+5)%6][1][0]
-	cube[(i+5)%6][1][0]=cube[(i+5)%6][2][0]
-	cube[(i+5)%6][2][0]=cube[(i+2)%6][0][2]
-	cube[(i+2)%6][0][2]=cube[(i+2)%6][0][1]
-	cube[(i+2)%6][0][1]=cube[(i+2)%6][0][0]
-	cube[(i+2)%6][0][0]=temp
-	 
-	# display()
+def rotate_back_anti():
+
+	i=0
+	temp = cube[i+1][0][0]
+	cube[i+1][0][0]=cube[i+1][1][0]
+	cube[i+1][1][0]=cube[i+1][2][0]
+	cube[i+1][2][0]=cube[i+1][2][1]
+	cube[i+1][2][1]=cube[i+1][2][2]
+	cube[i+1][2][2]=cube[i+1][1][2]
+	cube[i+1][1][2]=cube[i+1][0][2]
+	cube[i+1][0][2]=cube[i+1][0][1]
+	cube[i+1][0][1]=temp
+
+	for a in range(3):
+		temp = cube[i+4][0][2]
+		cube[i+4][0][2]=cube[i+4][0][1]
+		cube[i+4][0][1]=cube[i+4][0][0]
+		cube[i+4][0][0]=cube[i+5][2][2]
+		cube[i+5][2][2]=cube[i+5][1][2]
+		cube[i+5][1][2]=cube[i+5][0][2]
+		cube[i+5][0][2]=cube[i+2][2][2]
+		cube[i+2][2][2]=cube[i+2][2][1]
+		cube[i+2][2][1]=cube[i+2][2][0]
+		cube[i+2][2][0]=cube[i+3][0][0]
+		cube[i+3][0][0]=cube[i+3][1][0]
+		cube[i+3][1][0]=cube[i+3][2][0]
+		cube[i+3][2][0]=temp
+
+
+
+def rotate_back_clock():
+	# phase(anti-clockwise)
+	i=0
+	temp = cube[i+1][0][0]
+	cube[i+1][0][0]=cube[i+1][0][1]
+	cube[i+1][0][1]=cube[i+1][0][2]
+	cube[i+1][0][2]=cube[i+1][1][2]
+	cube[i+1][1][2]=cube[i+1][2][2]
+	cube[i+1][2][2]=cube[i+1][2][1]
+	cube[i+1][2][1]=cube[i+1][2][0]
+	cube[i+1][2][0]=cube[i+1][1][0]
+	cube[i+1][1][0]=temp
+
+	for a in range(3):
+		temp = cube[i+2][2][2]
+		cube[i+2][2][2]=cube[i+2][2][1]
+		cube[i+2][2][1]=cube[i+2][2][0]
+		cube[i+2][2][0]=cube[i+5][2][2]
+		cube[i+5][2][2]=cube[i+5][1][2]
+		cube[i+5][1][2]=cube[i+5][0][2]
+		cube[i+5][0][2]=cube[i+4][0][2]
+		cube[i+4][0][2]=cube[i+4][0][1]
+		cube[i+4][0][1]=cube[i+4][0][0]
+		cube[i+4][0][0]=cube[i+3][0][0]
+		cube[i+3][0][0]=cube[i+3][1][0]
+		cube[i+3][1][0]=cube[i+3][2][0]
+		cube[i+3][2][0]=temp
+
+
+def rotate_anti_mid_row():
+	i=0	
+	for a in range(3):
+		temp = cube[i+4][1][2]
+		cube[i+4][1][2]=cube[i+4][1][1]
+		cube[i+4][1][1]=cube[i+4][1][0]
+		cube[i+4][1][0]=cube[i+3][0][1]
+		cube[i+3][0][1]=cube[i+3][1][1]
+		cube[i+3][1][1]=cube[i+3][2][1]
+		cube[i+3][2][1]=cube[i+2][1][0]
+		cube[i+2][1][0]=cube[i+2][1][1]
+		cube[i+2][1][1]=cube[i+2][1][2]
+		cube[i+2][1][2]=cube[i+5][2][1]
+		cube[i+5][2][1]=cube[i+5][1][1]
+		cube[i+5][1][1]=cube[i+5][0][1]
+		cube[i+5][0][1]=temp
+
+def rotate_clock_mid_row():
+	i=0	
+	for a in range(3):
+		temp = cube[i+4][1][0]
+		cube[i+4][1][0]=cube[i+4][1][1]
+		cube[i+4][1][1]=cube[i+4][1][2]
+		cube[i+4][1][2]=cube[i+5][0][1]
+		cube[i+5][0][1]=cube[i+5][1][1]
+		cube[i+5][1][1]=cube[i+5][2][1]
+		cube[i+5][2][1]=cube[i+2][1][2]
+		cube[i+2][1][2]=cube[i+2][1][1]
+		cube[i+2][1][1]=cube[i+2][1][0]
+		cube[i+2][1][0]=cube[i+3][2][1]
+		cube[i+3][2][1]=cube[i+3][1][1]
+		cube[i+3][1][1]=cube[i+3][0][1]
+		cube[i+3][0][1]=temp
 
 
 
@@ -105,7 +530,6 @@ test = ImageTk.PhotoImage(image1)
 canvas.create_image(0, 0, image=test, anchor='nw')
 
 
-
 cube = valid_cube_generation()
 
 
@@ -113,17 +537,30 @@ cube = valid_cube_generation()
 # button.place(x=100,y=100)
 
 
-rotation_by_phase_clock()
-
-
-
-
-
+rotate_up_column_0()
+rotate_down_column_0()
+rotate_up_column_2()
+rotate_down_column_2()
+rotate_up_column_1()
+rotate_down_column_1()
+rotate_right_row_0()
+rotate_left_row_0()
+rotate_right_row_2()
+rotate_left_row_2()
+rotate_left_row_1()
+rotate_right_row_1()
+rotate_front_clock()
+rotate_front_anti()
+rotate_back_clock()
+rotate_back_anti()
+rotate_clock_mid_row()
+rotate_anti_mid_row()
 
 for i in range(6):
 	print(cube[i])
 
 display()
+
 
 canvas.pack()
 
